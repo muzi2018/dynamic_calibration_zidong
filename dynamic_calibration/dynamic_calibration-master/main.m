4d290ca390a48ffa9a328fdcdfd644284e35c9ce
Weijie_Wang <3127870356@qq.com>
2022-08-14 19:38:49 +0800

    nothing

--- dynamic_calibration/dynamic_calibration-master/main.m
+++ dynamic_calibration/dynamic_calibration-master/main.m
@@ -3,7 +3,6 @@ clear all; close all; clc;
 % Define path to a urdf file
 path_to_urdf = 'ur10e.urdf';
 
-
 % Generate functions for dynamics based on Lagrange method
 % Note that it might take some time
 % generate_rb_dynamics(path_to_urdf);
--- dynamic_calibration/dynamic_calibration-master/main_leg.m
+++ dynamic_calibration/dynamic_calibration-master/main_leg.m
@@ -2,8 +2,9 @@ clear all; close all; clc;
 
 % Define path to a urdf file
 path_to_urdf = 'leg.urdf';
+ur10 = parse_urdf(path_to_urdf);
 % generate_legs_regressor(path_to_urdf);  
-generate_legs_regressor(path_to_urdf);
+% generate_legs_regressor(path_to_urdf);
 % Perform QR decompostion in order to get base parameters of the robot
 include_motor_dynamics = 0;
 [pi_lgr_base, baseQR] = base_params_qr_leg(include_motor_dynamics);
@@ -24,6 +25,17 @@ load('path_to_val_data.mat');
 idxs = [700, 4200];
 rre = validate_dynamic_params_leg(path_to_val_data, idxs, ...
                               drive_gains, baseQR, sol.pi_b, sol.pi_fr);
+
+beta = baseQR.beta;
+bb = baseQR.numberOfBaseParameters;
+E = baseQR.permutationMatrix;
+pi_full(1:10,1)=ur10.pi(1:10,1);
+pi_full(11:20,1)=ur10.pi(1:10,2);
+pi_full(21:30,1)=ur10.pi(1:10,3);
+pi_full(31:40,1)=ur10.pi(1:10,4);
+pi_lgr_base = [eye(bb) beta]*E'*ur10.pi;
+
+
 E1=baseQR.permutationMatrix(:,1:baseQR.numberOfBaseParameters);
 pi=E1*sol.pi_b;
 
