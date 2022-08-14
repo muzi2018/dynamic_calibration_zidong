bbdcca053555d916fc79db3c27a3860f824086e8
Weijie_Wang <3127870356@qq.com>
2022-08-14 18:40:35 +0800

    nothing

--- dynamic_calibration/dynamic_calibration-master/main_leg.m
+++ dynamic_calibration/dynamic_calibration-master/main_leg.m
@@ -3,7 +3,7 @@ clear all; close all; clc;
 % Define path to a urdf file
 path_to_urdf = 'leg.urdf';
 % generate_legs_regressor(path_to_urdf);  
-
+generate_legs_regressor(path_to_urdf);
 % Perform QR decompostion in order to get base parameters of the robot
 include_motor_dynamics = 0;
 [pi_lgr_base, baseQR] = base_params_qr_leg(include_motor_dynamics);
